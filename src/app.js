49a52bafeca5fa4c61dfcd9218988e508c6a7750
Mermate <138124998+Mermate@users.noreply.github.com>
2023-10-08 18:01:20 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,19 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("excuse").innerHTML = excuseGenerator();
 };
+
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car", "the book"];
+let when = ["before lunch", "when I finished", " last Sunday"];
+
+function excuseGenerator() {
+  let whorandom = who[Math.floor(Math.random() * who.length)];
+  let actionrandom = action[Math.floor(Math.random() * action.length)];
+  let whatrandom = what[Math.floor(Math.random() * what.length)];
+  let whenrandom = when[Math.floor(Math.random() * when.length)];
+
+  return whorandom + " " + actionrandom + " " + whatrandom + " " + whenrandom;
+}
